b60560ec78f965f26274bd5acbe0cebb42bc9276
gbrs <gbrs@mail.ru>
2020-12-08 21:42:50 +0300

    сделал для 9.3 транспонирование

--- solution.py
+++ solution.py
@@ -3,6 +3,16 @@ from sys import stdin
 
 class Matrix:
 
+    @staticmethod
+    def transposed(matrix):
+        new_matrix = []
+        for j in range(len(matrix.lst[0])):
+            inner_list = []
+            for i in range(len(matrix.lst)):
+                inner_list.append(matrix.lst[i][j])
+            new_matrix.append(inner_list)
+        return Matrix(new_matrix)
+
     def __init__(self, lst=[]):
         copy_outer_list = []
         for i in range(len(lst)):
@@ -27,8 +37,18 @@ class Matrix:
     def size(self):
         return len(self.lst), len(self.lst[0])
 
+    def transpose(self):
+        new_matrix = []
+        for j in range(len(self.lst[0])):
+            inner_list = []
+            for i in range(len(self.lst)):
+                inner_list.append(self.lst[i][j])
+            new_matrix.append(inner_list)
+        self.lst = new_matrix
+        return self
 
-exec(stdin.read())
+
+# exec(stdin.read())
 
 # m = Matrix([[1, 0], [0, 1]])
 # print(m)
@@ -48,3 +68,26 @@ exec(stdin.read())
 # m = Matrix([[1, 2]])
 # print(str(m) == '1\t1\t1')
 # print(m.size())
+
+# m1 = Matrix([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+# m2 = Matrix([[0, 1, 0], [20, 0, -1], [-1, -2, 0]])
+# print(m1 + m2)
+#
+# m2 = Matrix([[0, 1, 0], [20, 0, -1]])
+# try:
+#     m = m1 + m2
+#     print('WA\n' + str(m))
+# except MatrixError as e:
+#     print(e.matrix1)
+#     print(e.matrix2)
+
+# m = Matrix([[10, 10], [0, 0], [1, 1]])
+# print(m)
+# m1 = m.transpose()
+# print(m)
+# print(m1)
+
+m = Matrix([[10, 10], [0, 0], [1, 1]])
+print(m)
+print(Matrix.transposed(m))
+print(m)
