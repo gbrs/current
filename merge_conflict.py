68330b59bd773d9f5588eb1bdf86bd74e52fe51d
gbrs <gbrs@mail.ru>
2023-01-25 00:16:41 +0300

    experiment with merge conflict

---
+++ merge_conflict.py
@@ -0,0 +1,10 @@
+from sys import getsizeof
+
+lst = [1, 2, 3, 4]
+print(getsizeof(lst))
+lst = ['dsjfhglkdha', 'sadhfkjls', 'adhfkjsdhsdafgdsgs', 'sdgakjdfg']
+print(getsizeof(lst))
+lst = [[1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4]]
+print(getsizeof(lst))
+lst = [[1, 2, 3, 4], 'fdgfd', 5, {x: x ** 2 for x in range(1000)}]
+print(getsizeof(lst))
