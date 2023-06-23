295f258ed4bac74bc505c60e64814e48538e8dc6
gbrs <61385511+gbrs@users.noreply.github.com>
2023-06-23 11:48:38 +0300

    solve task 3.2.2

--- regex_in_python.py
+++ regex_in_python.py
@@ -1,15 +1,8 @@
 import re
 
-reg = r'\b[А-Яа-я]+\b'
-s = 'Второй тест'
-match = re.match(reg, s)
-print(match.group(0))
-print(match.start())
-print(match.end())
-print(match.pos)
-print(match.endpos)
-print(match.re)
-print(match.string)
+match = re.match(input(), input())
+if match:
+    print(match.group(0))
 
 
 """
@@ -18,7 +11,6 @@ https://stepik.org/course/107335/syllabus
 ----------------
 3.2.1
 import re
-
 reg = r'\b[А-Яа-я]+\b'
 s = 'Второй тест'
 match = re.match(reg, s)
@@ -30,25 +22,13 @@ print(match.endpos)
 print(match.re)
 print(match.string)
 
-----------------
+
+3.2.2
+import re
+match = re.match(input(), input())
+if match:
+    print(match.group(0))
 
 
 
-----------------
-
-
-
-----------------
-
-
-
-----------------
-
-
-
-----------------
-
-
-
-----------------
 """
