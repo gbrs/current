54c6d3da3aca820115da0489546992d9b78fa8b9
gbrs <gbrs@mail.ru>
2023-07-02 13:24:13 +0300

    solve task 3.3.1

--- regex_in_python.py
+++ regex_in_python.py
@@ -1,8 +1,19 @@
 import re
 
-match = re.match(input(), input())
-if match:
-    print(match.group(0))
+# сделать список строк?
+text = ''
+for _ in range(4):
+    text = text + input() + '\n'
+
+print(text)
+
+search = re.search(r'[Кк]од', text)
+print(search)
+if search:
+    print(search.span()[0])
+else:
+    print('код не найден')
+
 
 
 """
@@ -28,6 +39,18 @@ import re
 match = re.match(input(), input())
 if match:
     print(match.group(0))
+
+
+3.3.1
+import re
+
+search = re.search(r'#[a-z]+', input())
+if search:
+    print(search.group(0))
+
+
+3.3.2
+
 
 
 
