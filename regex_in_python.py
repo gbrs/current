897368385bae5380001125c39d2b89eb409ad001
gbrs <gbrs@mail.ru>
2023-10-15 00:05:02 +0300

    solve task 3.6.3

--- regex_in_python.py
+++ regex_in_python.py
@@ -1,17 +1,15 @@
 import re
 
-s = '''Мы вынуждены отталкиваться от того, что разбавленное изрядной долей эмпатии, 
-        рациональное мышление способствует подготовке и реализации модели развития. 
-        В целом, конечно, консультация с широким активом предоставляет широкие 
-        возможности для форм воздействия.'''
+s = '''</div></div><div class="main-indicator_rate"><div class="col-md-2 col-xs-9 _dollar">USD</div><div class="col-md-2 col-xs-9 _right mono-num">74,9990 ₽</div><div class="col-md-2 col-xs-9 _right mono-num">73,5050 ₽</div><div class="main-indicator_tooltip" id="V_R01235"><div class="main-indicator_tooltip-head"><button class="main-indicator_tooltip-head-btn _left "></button><div class="main-indicator_tooltip-head-text">19.04.2022 - 23.04.2022</div><button class="main-indicator_tooltip-head-btn _right _disabled "></button></div><table class="main-indicator_tooltip-table"><tr><td class="_day">вт</td><td class="_date">19.04</td><td>79,4529&nbsp;₽</td><td class="_green">-0,5908&nbsp;₽</td></tr><tr><td class="_day">ср</td><td class="_date">20.04</td><td>79,0287&nbsp;₽</td><td class="_green">-0,4242&nbsp;₽</td></tr><tr><td class="_day">чт</td><td class="_date">21.04</td><td>77,0809&nbsp;₽</td><td class="_green">-1,9478&nbsp;₽</td></tr><tr><td class="_day">пт</td><td class="_date">22.04</td><td>74,9990&nbsp;₽</td><td class="_green">-2,0819&nbsp;₽</td></tr><tr><td class="_day">сб</td><td class="_date">23.04</td><td>73,5050&nbsp;₽</td><td class="_green">-1,4940&nbsp;₽</td></tr></table><div class="main-indicator_tooltip-footer">Официальный курс Банка России</div></div></div><div class="main-indicator_rate"><div class="col-md-2 col-xs-9 _euro">EUR</div><div class="col-md-2 col-xs-9 _right mono-num">81,2239 ₽</div><div class="col-md-2 col-xs-9 _right mono-num">80,0249 ₽</div>'''
 
-answer = re.finditer(r"\b[A-Za-zА-Яа-яЁё]{5}\b", s)
+answer = re.finditer(r"\d+,\d+ ₽", s)
 
 for word in answer:
     print(word.group(0))
 
-print(*answer, sep='\n')  # другой вариант
-
+# лепота:
+# for i in re.finditer(r'\d{2,},\d{4} ₽', input()):
+#     print(i[0])
 
 """
 Код из курса "Регулярные выражения в Python"
@@ -128,9 +126,33 @@ answer = re.finditer(r"\w+", s)
 for word in answer:
     print(word.group(0))
 
+3.6.2
+import re
 
+s = '''Мы вынуждены отталкиваться от того, что разбавленное изрядной долей эмпатии, 
+        рациональное мышление способствует подготовке и реализации модели развития. 
+        В целом, конечно, консультация с широким активом предоставляет широкие 
+        возможности для форм воздействия.'''
 
+answer = re.finditer(r"\b[A-Za-zА-Яа-яЁё]{5}\b", s)
 
+for word in answer:
+    print(word.group(0))
 
+print(*answer, sep='\n')  # другой вариант
+
+3.6.3
+import re
+
+s = '''</div></div><div class="main-indicator_rate"><div class="col-md-2 col-xs-9 _dollar">USD</div><div class="col-md-2 col-xs-9 _right mono-num">74,9990 ₽</div><div class="col-md-2 col-xs-9 _right mono-num">73,5050 ₽</div><div class="main-indicator_tooltip" id="V_R01235"><div class="main-indicator_tooltip-head"><button class="main-indicator_tooltip-head-btn _left "></button><div class="main-indicator_tooltip-head-text">19.04.2022 - 23.04.2022</div><button class="main-indicator_tooltip-head-btn _right _disabled "></button></div><table class="main-indicator_tooltip-table"><tr><td class="_day">вт</td><td class="_date">19.04</td><td>79,4529&nbsp;₽</td><td class="_green">-0,5908&nbsp;₽</td></tr><tr><td class="_day">ср</td><td class="_date">20.04</td><td>79,0287&nbsp;₽</td><td class="_green">-0,4242&nbsp;₽</td></tr><tr><td class="_day">чт</td><td class="_date">21.04</td><td>77,0809&nbsp;₽</td><td class="_green">-1,9478&nbsp;₽</td></tr><tr><td class="_day">пт</td><td class="_date">22.04</td><td>74,9990&nbsp;₽</td><td class="_green">-2,0819&nbsp;₽</td></tr><tr><td class="_day">сб</td><td class="_date">23.04</td><td>73,5050&nbsp;₽</td><td class="_green">-1,4940&nbsp;₽</td></tr></table><div class="main-indicator_tooltip-footer">Официальный курс Банка России</div></div></div><div class="main-indicator_rate"><div class="col-md-2 col-xs-9 _euro">EUR</div><div class="col-md-2 col-xs-9 _right mono-num">81,2239 ₽</div><div class="col-md-2 col-xs-9 _right mono-num">80,0249 ₽</div>'''
+
+answer = re.finditer(r"\d+,\d+ ₽", s)
+
+for word in answer:
+    print(word.group(0))
+
+# лепота:
+# for i in re.finditer(r'\d{2,},\d{4} ₽', input()):
+#     print(i[0])
 
 """
