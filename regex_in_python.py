e1c12463bf6cd81cfc4f7ee0aacc34b1ec6ab196
gbrs <gbrs@mail.ru>
2024-01-01 01:13:11 +0300

    solve task 3.9.2

--- regex_in_python.py
+++ regex_in_python.py
@@ -1,7 +1,6 @@
 import re
-
-print(re.split(r'Категория: [А-ЯЁа-яё ]+\\n', 'Категория: Телефоны\nSupreme Burner\nMotorola Razr\nКатегория: Смарт часы и браслеты\nApple Watch 6\nGarmin Venu\nXiaomi Mi Smart Band 6\nКатегория: Игры\nSpore'))
-print(re.split(r'Категория: [А-ЯЁа-яё ]+\\n',  input()))
+s = '''<html lang="en"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Timer ⏲</title><link rel="icon" href="./img/goes.png"><link rel="stylesheet" href="./css/normalize.css"><link rel="stylesheet" href="./css/style.css"></head><body><div class="time_wrapper"><h1 class="bold minutes">1:00:00</h1><img class="time" src="./img/start_end.png"></div><div class="buttons"><button class="buttons_button regular start" onclick="start()">Start</button><button class="buttons_button regular notshow pause" onclick="pause()">Pause</button></div></body>'''
+print(''.join(re.split(r"<.*?>", s)))
 
 
 
@@ -214,4 +213,10 @@ print(re.split(r"Категория: [А-Яа-яЁё ]+\n?", s))
 import re
 s = input()
 print(re.split(r"Категория: [А-Яа-яЁё ]+\\n", s))
+
+
+3.9.2
+import re
+s = '''<html lang="en"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Timer ⏲</title><link rel="icon" href="./img/goes.png"><link rel="stylesheet" href="./css/normalize.css"><link rel="stylesheet" href="./css/style.css"></head><body><div class="time_wrapper"><h1 class="bold minutes">1:00:00</h1><img class="time" src="./img/start_end.png"></div><div class="buttons"><button class="buttons_button regular start" onclick="start()">Start</button><button class="buttons_button regular notshow pause" onclick="pause()">Pause</button></div></body>'''
+print(''.join(re.split(r"<.*?>", s)))
 """
