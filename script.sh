313be61112b31628ed9ec059672b1ec26075deaf
gbrs <gbrs@mail.ru>
2024-06-29 23:09:45 +0300

    create file script.sh

---
+++ script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+while true
+do
+    read var1
+    if [ $var1 == exit ];
+    then
+        echo bye
+        break
+    fi
+
+
+    #gcd $var1 $var2
+    #313788397 2546698687
+    #echo GCD is $answer
+    #read var1 var2
+done
