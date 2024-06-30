38ee1a18e99fc7bf173c6e9b7193cc54558d7ba8
gbrs <gbrs@mail.ru>
2024-06-30 11:53:56 +0300

    start calculator code

--- script.sh
+++ script.sh
@@ -3,14 +3,25 @@
 
 while true
 do
-    read var1
-    if [ $var1 == exit ];
-    then
-        echo bye
-        break
+  read var1
+  if [[ $var1 == exit ]]; then
+    echo bye
+    break
+  elif [[ $var1 =~ ^[0-9]+$ ]]; then
+    read var2 var3
+    if [[ $var2 == + ]]; then
+      #echo 5
+      #read var4
+      let ans=$var1+$var3
+      echo $ans
+      continue
     fi
+  else
+    echo error
+    break
+  fi
 
-
+    #"+", "-", "*", "/", "%", "**"
     #gcd $var1 $var2
     #313788397 2546698687
     #echo GCD is $answer
