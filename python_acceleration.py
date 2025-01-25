eba3510315d058b720d79516fe2cf19f251f3642
gbrs <gbrs@mail.ru>
2025-01-25 12:51:37 +0300

    experiment with setrecursionlimit and threading

---
+++ python_acceleration.py
@@ -0,0 +1,30 @@
+
+
+
+
+
+'''
+-----------------
+
+from sys import setrecursionlimit
+import threading
+
+
+def factorial(n, mod):
+    if n == 0:
+        return 1
+    return (n * factorial(n - 1, mod)) % mod
+
+
+def main():  # наша программа
+    mod = 10**9 + 7
+    n = int(input())
+    res = factorial(n, mod)
+    print(res)
+
+
+setrecursionlimit(10 ** 9)
+threading.stack_size(2 ** 26)  # лучше использовать именно эту константу
+thread = threading.Thread(target=main)
+thread.start()
+'''
